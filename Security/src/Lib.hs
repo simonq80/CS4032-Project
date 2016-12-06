dba53e0b5ab6ea7f249269f7cf3868079ff81aa1
root <root@LocalDeliveryOnly>
2016-12-06 17:21:53 +0000

    testing

--- Security/src/Lib.hs
+++ Security/src/Lib.hs
@@ -10,16 +10,17 @@ import Data.Aeson.TH
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
+import Data.List.Split
+import Control.Monad.IO.Class
 
 data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
+  { userName :: String
+  , password :: String
   } deriving (Eq, Show)
 
 $(deriveJSON defaultOptions ''User)
 
-type API = "users" :> Get '[JSON] [User]
+type API = Capture "u" String :> Capture "p" String :> Get '[JSON] [User]
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -31,9 +32,21 @@ api :: Proxy API
 api = Proxy
 
 server :: Server API
-server = return users
+server = userList
 
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+userList :: String -> String -> Handler [User]
+userList u p = do
+    u <- liftIO $ users u p
+    return u
+
+users :: String -> String -> IO [User]
+users u p = do
+    users <- readFile "/Distro/Security/users.txt"
+    return $ filter (userFilter u p) (map toUser (lines users))
+
+toUser :: String -> User
+toUser s = User (head split) (head $ tail split) where
+    split = splitOn ":" s
+
+userFilter :: String -> String -> User -> Bool
+userFilter u p s = (u == userName s) && (p == password s)
